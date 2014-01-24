a1a1fd5ed62820ae4162c06358a386e69aff0750
adrain <adrianduignan101@yahoo.com>
2014-01-24 12:09:38 +0000

    java application
    The Fibonacci Sequence is the series of numbers and this is done by  by
    adding up the two numbers before it.

---
+++ src/fibonacci.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class fibonacci {
+
+	public static void main(String[] args) { 
+		Scanner input = new Scanner(System.in);
+		int num1=0,num2=1,sum;
+		int count;
+		
+		System.out.println("this program counts the number" +
+				" of digits in a integer entered by user");
+		
+		System.out.println("enter N");
+		int N = input.nextInt();
+		System.out.println("the first " + N  + " numbers in the fibonacci" +
+				"sequence are as follows");
+		
+		
+		
+
+		for (count = 1;count<= N; count++) { 
+		sum=num1+num2; 
+		num1=num2; 
+		num2=sum; 
+		System.out.print(" "+num1); 
+		} 
+		} 
+		} 
+
